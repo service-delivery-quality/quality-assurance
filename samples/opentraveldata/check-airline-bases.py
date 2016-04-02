f1f84828986bbc9084f87873ffb87cdfbac15dea
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2016-04-03 00:40:07 +0200

    [Samples][OpenTravelData] Fixed a few typos in the airline base checker.

--- samples/opentraveldata/check-airline-bases.py
+++ samples/opentraveldata/check-airline-bases.py
@@ -98,6 +98,8 @@ if __name__ == '__main__':
     displayFileHead (optd_airline_por_file)
 
   #
+  # airline_code^apt_org^apt_dst^flt_freq
+  #
   airline_dict = dict()
   with open (optd_airline_por_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
@@ -106,46 +108,60 @@ if __name__ == '__main__':
       apt_org = row['apt_org']
       apt_dst = row['apt_dst']
       flt_freq = row['flt_freq']
+
       # Register or update a dictionary for that airline code
+      airline_por_list = dict([(apt_org, flt_freq), (apt_dst, flt_freq)])
       if not airline_code in airline_dict:
         # Register the flight frequencies for the origin and destination airports
-        airline_dict[airline_code] = dict([(apt_org, flt_freq), (apt_dst, flt_freq)])
+        airline_dict[airline_code] = airline_por_list
       else:
         airline_por_list = airline_dict[airline_code]
-        # Register the flight frequency for the origin airport
-        if not apt_org in airline_por_list:
-          airline_por_list[apt_org] = int(flt_freq)
-        else:
-          cumulated_flt_freq = int(airline_por_list[apt_org])
-          airline_por_list[apt_org] = cumulated_flt_freq + int(flt_freq)
-        # Register the flight frequency for the destination airport
-        if not apt_dst in airline_por_list:
-          airline_por_list[apt_dst] = int(flt_freq)
-        else:
-          cumulated_flt_freq = int(airline_por_list[apt_dst])
-          airline_por_list[apt_dst] = cumulated_flt_freq + int(flt_freq)
 
+      # Register the flight frequency for the origin airport
+      if not apt_org in airline_por_list:
+        airline_por_list[apt_org] = int(flt_freq)
+      else:
+        cumulated_flt_freq = int(airline_por_list[apt_org])
+        airline_por_list[apt_org] = cumulated_flt_freq + int(flt_freq)
+      # Register the flight frequency for the destination airport
+      if not apt_dst in airline_por_list:
+        airline_por_list[apt_dst] = int(flt_freq)
+      else:
+        cumulated_flt_freq = int(airline_por_list[apt_dst])
+        airline_por_list[apt_dst] = cumulated_flt_freq + int(flt_freq)
+
+  #
+  # pk^env_id^validity_from^validity_to^3char_code^2char_code^num_code^name^name2^alliance_code^alliance_status^type^wiki_link^flt_freq^alt_names^bases
   #
   with open (optd_airline_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
     for row in file_reader:
-      airline_code = row['2char_code']
       pk = row['pk']
+      airline_code = row['2char_code']
+      env_id = row['env_id']
+
       # Register or update a dictionary for that airline code
+      airline_por_list = dict([(pk ,1)])
       if not airline_code in airline_dict:
-        airline_dict[airline_code] = dict([(pk ,1)])
+        airline_dict[airline_code] = airline_por_list
+
       else:
         airline_por_list = airline_dict[airline_code]
-        airline_por_list[pk] = 1
-        base_list_str = row['bases']
-        base_list = base_list_str.split('=')
-        # print (airline_code + ": " + base_list_str + " (" + str(base_list) + ")")
-        # Check whether the airport bases/hubs appear in the file of POR list
-        if base_list:
-          for base in base_list:
-            if base and not base in airline_por_list:
-              reportStruct = {'airline_code': airline_code, 'base': base}
-              print (str(reportStruct))
+
+      # Register the airport hubs/bases
+      airline_por_list[pk] = 1
+
+      base_list_str = row['bases']
+      base_list = base_list_str.split('=')
+      # print (airline_code + ": " + base_list_str + " (" + str(base_list) + ")")
+
+      # Check, if the airline is still active,
+      # whether the airport bases/hubs appear in the file of POR list
+      if base_list and env_id == '':
+        for base in base_list:
+          if base and not base in airline_por_list:
+            reportStruct = {'airline_code': airline_code, 'base': base}
+            print (str(reportStruct))
 
   # DEBUG
   if verboseFlag:
