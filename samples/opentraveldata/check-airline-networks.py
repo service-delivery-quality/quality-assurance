1043f15283ec30b6890acbe9fe5f6549747839c9
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2016-05-02 00:17:14 +0200

    [Samples][OpenTravelData] Improved version of the airline network checker.

--- samples/opentraveldata/check-airline-networks.py
+++ samples/opentraveldata/check-airline-networks.py
@@ -1,111 +1,20 @@
 #!/usr/bin/python3
 
-import urllib.request, shutil, csv, datetime, re, getopt, sys, os
+import csv, re
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import DeliveryQuality as dq
 
-# Index increment
-k_idx_inc = 100
-k_ascii = 64
-
-# Usage
-def usage (script_name):
-  print ("")
-  print ("Usage: %s [options]" % script_name)
-  print ("")
-  print ("That script downloads OpenTravelData (OPTD) airline-related CSV files")
-  print ("and check outliers within airline networks")
-  print ("")
-  print ("Options:")
-  print ("  -h, --help      : outputs this help and exits")
-  print ("")
-
-# Handle command-line options
-def handle_opt():
-  try:
-    opts, args = getopt.getopt (sys.argv[1:], "hv", ["help", "verbose"])
-  except (getopt.GetoptError, err):
-    # Print help information and exit. It will print something like
-    # "option -a not recognized"
-    print (str (err))
-    usage()
-    sys.exit(2)
-
-  # Options
-  verboseFlag = False
-  for o, a in opts:
-    if o in ("-h", "--help"):
-      usage (sys.argv[0])
-      sys.exit()
-    elif o in ("-v", "--verbose"):
-      verboseFlag = True
-    else:
-      assert False, "Unhandled option"
-  return (verboseFlag)
-
-# Download a file from an URL
-def downloadFile (file_url, output_file, verbose_flag):
-  if verbose_flag:
-    print ("Downloading '" + output_file + "' from " + file_url + "...")
-  with urllib.request.urlopen (file_url) as response, open (output_file, 'wb') as out_file:
-    shutil.copyfileobj (response, out_file)
-  if verbose_flag:
-    print ("... done")
-  return
-
-# Donwload a file if needed
-def downloadFileIfNeeded (file_url, output_file, verbose_flag):
-  # Check whether the output_file has already been downloaded
-  try:
-    if os.stat (output_file).st_size > 0:
-      file_time = datetime.datetime.fromtimestamp (os.path.getmtime (output_file))
-      if verbose_flag:
-        print ("Time-stamp of '" + output_file + "': " + str(file_time))
-        print ("If that file is too old, you can delete it, and re-execute that script")
-    else:
-      downloadFile (file_url, output_file)
-  except OSError:
-    downloadFile (file_url, output_file)
-  return
-
-# Display the first 10 lines of a CSV file
-def displayFileHead (input_file):
-  #
-  print ("Header of the '" + input_file + "' file")
-  #
-  with open (input_file, newline='') as csvfile:
-    file_reader = csv.reader (csvfile, delimiter='^')
-    for i in range(10):
-      print (','.join(file_reader.__next__()))
-
-  #
-  return
-
-# Derive an index from a IATA code
-def getIdx (iata_code):
-    idx = 0
-    if (len(iata_code) != 3):
-        idx = 0
-    ic1 = (ord(iata_code[0]) - k_ascii) * k_idx_inc**2
-    ic2 = (ord(iata_code[1]) - k_ascii) * k_idx_inc
-    ic3 = ord(iata_code[2]) - k_ascii
-    idx = ic1 + ic2 + ic3
-    return idx
-
-# Derive the IATA code from an index
-def getIataCode (idx):
-    ic1 = int(idx / k_idx_inc**2)
-    ic2 = int((idx - ic1 * k_idx_inc**2) / k_idx_inc)
-    ic3 = idx - (ic1 * k_idx_inc**2 + ic2 * k_idx_inc)
-    iata_code = chr(ic1 + k_ascii) + chr(ic2 + k_ascii) + chr(ic3 + k_ascii)
-    return iata_code
-
+#
+k_airline_idx = 10000
 
 # Main
 if __name__ == '__main__':
   #
-  verboseFlag = handle_opt()
+  usageStr = "That script downloads OpenTravelData (OPTD) airline-related CSV files\nand check outliers within airline networks"
+  verboseFlag = dq.handle_opt(usageStr)
 
   # OPTD-maintained list of POR, master file
   optd_por_bksf_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/opentraveldata/optd_por_best_known_so_far.csv?raw=true'
@@ -120,25 +29,23 @@ if __name__ == '__main__':
   optd_airline_por_file = 'to_be_checked/optd_airline_por.csv'
 
   # If the files are not present, or are too old, download them
-  downloadFileIfNeeded (optd_por_bksf_url, optd_por_bksf_file, verboseFlag)
-  downloadFileIfNeeded (optd_airline_url, optd_airline_file, verboseFlag)
-  downloadFileIfNeeded (optd_airline_por_url, optd_airline_por_file, verboseFlag)
+  dq.downloadFileIfNeeded (optd_por_bksf_url, optd_por_bksf_file, verboseFlag)
+  dq.downloadFileIfNeeded (optd_airline_url, optd_airline_file, verboseFlag)
+  dq.downloadFileIfNeeded (optd_airline_por_url, optd_airline_por_file, verboseFlag)
 
   # DEBUG
   if verboseFlag:
-    displayFileHead (optd_por_bksf_file)
-    displayFileHead (optd_airline_file)
-    displayFileHead (optd_airline_por_file)
+    dq.displayFileHead (optd_por_bksf_file)
+    dq.displayFileHead (optd_airline_file)
+    dq.displayFileHead (optd_airline_por_file)
 
   #
   basemap = Basemap(projection='robin',lon_0=0,resolution='l')
-  basemap.drawcountries(linewidth = 0.5)
-  basemap.fillcontinents(color='white',lake_color='white')
-  basemap.drawcoastlines(linewidth=0.5)
 
   #
   # pk^iata_code^latitude^longitude^city_code^date_from
-  optd_por_dict = dict()
+  optd_por_map_dict = dict()
+  optd_por_coord_dict = dict()
   primary_key_re = re.compile ("^([A-Z]{3})-([A-Z]{1,2})-([0-9]{1,15})$")
   with open (optd_por_bksf_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
@@ -153,14 +60,15 @@ if __name__ == '__main__':
       optd_bksf_date_from = row['date_from']
 
       # Register the POR coordinates, if it is seen for the first time
-      if not optd_bksf_iata_code in optd_por_dict:
-        optd_por_dict[optd_bksf_iata_code] = basemap(optd_bksf_coord_lon, optd_bksf_coord_lat)
+      if not optd_bksf_iata_code in optd_por_map_dict:
+        optd_por_coord_dict[optd_bksf_iata_code] = (optd_bksf_coord_lat, optd_bksf_coord_lon)
+        optd_por_map_dict[optd_bksf_iata_code] = basemap(optd_bksf_coord_lon, optd_bksf_coord_lat)
 
 
   #
   # airline_code^apt_org^apt_dst^flt_freq
   #
-  airline_dict = dict()
+  airline_sched_dict = dict()
   schedule_dict = dict()
   last_airline_code = ""
   with open (optd_airline_por_file, newline='') as csvfile:
@@ -183,11 +91,11 @@ if __name__ == '__main__':
       
       # Register or update a dictionary for that airline code
       airline_por_list = dict([(apt_org, flt_freq), (apt_dst, flt_freq)])
-      if not airline_code in airline_dict:
+      if not airline_code in airline_sched_dict:
         # Register the flight frequencies for the origin and destination airports
-        airline_dict[airline_code] = airline_por_list
+        airline_sched_dict[airline_code] = airline_por_list
       else:
-        airline_por_list = airline_dict[airline_code]
+        airline_por_list = airline_sched_dict[airline_code]
 
       # Register the flight frequency for the origin airport
       if not apt_org in airline_por_list:
@@ -204,23 +112,25 @@ if __name__ == '__main__':
 
   # DEBUG
   #nx.draw_graphviz (schedule_dict["FC"])
-  nx.draw_networkx (schedule_dict["FC"], optd_por_dict, node_color='blue')
-  nx.draw_networkx (schedule_dict["KT"], optd_por_dict, node_color='red')
-  plt.show()
+  #nx.draw_networkx (schedule_dict["FC"], optd_por_map_dict, node_color='blue')
+  #nx.draw_networkx (schedule_dict["KT"], optd_por_map_dict, node_color='red')
+  #plt.show()
   
   #
-  # pk^env_id^validity_from^validity_to^3char_code^2char_code^num_code^name^name2^alliance_code^alliance_status^type^wiki_link^flt_freq^alt_names^bases
+  # pk^env_id^validity_from^validity_to^3char_code^2char_code^num_code^name^name2^alliance_code^alliance_status^type^wiki_link^flt_freq^alt_names^bases^key^version
   #
+  airline_dict = dict()
   with open (optd_airline_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
     for row in file_reader:
       pk = row['pk']
       airline_code = row['2char_code']
+      airline_name = row['name']
       env_id = row['env_id']
 
       # Register the details for that airline code
-      if airline_code in airline_dict:
-        airline_por_list = airline_dict[airline_code]
+      if airline_code in airline_sched_dict:
+        airline_por_list = airline_sched_dict[airline_code]
 
         # Register the airport hubs/bases
         airline_por_list[pk] = 1
@@ -237,27 +147,122 @@ if __name__ == '__main__':
               reportStruct = {'airline_code': airline_code, 'base': base}
               print (str(reportStruct))
 
+      # Register the details for the airline
+      airline_dict[airline_code] = {'airline_code': airline_code,
+                                    'airline_name': airline_name}
+
+
   # DEBUG
   if verboseFlag:
-    print ("Airline full dictionary:\n" + str(airline_dict))
+    print ("Airline full dictionary:\n" + str(airline_sched_dict))
 
 
   # http://stackoverflow.com/questions/19915266/drawing-a-graph-with-networkx-on-a-basemap
 # Networks on Maps: http://www.sociology-hacks.org/?p=67
   # Creating a route planner for road network: http://ipython-books.github.io/featured-03/
   
-  #
-  basemap = Basemap(projection='robin',lon_0=0,resolution='l')
-  basemap.drawcountries(linewidth = 0.5)
-  basemap.fillcontinents(color='white',lake_color='white')
-  basemap.drawcoastlines(linewidth=0.5)
-
   # Decompose the airline network into independent sub-networks
-  graphs = list(nx.connected_component_subgraphs(schedule_dict["K5"]))
-  for graph_comp in graphs:
-      # Find the center of the sub-network
-      graph_comp_center = nx.center(graph_comp)
-      print ("Center: " + str(graph_comp_center))
-      nx.draw_networkx (graph_comp, optd_por_dict, node_color='green')
-      plt.show()
+  airline_idx = 1
+  #for airline_code in ("K5", "U2", "9K", "LH"):
+  for airline_code in airline_sched_dict:
+      graph_comp_idx = 1
+      graphs = list(nx.connected_component_subgraphs(schedule_dict[airline_code]))
+      for graph_comp in graphs:
+          # Find the center of the sub-network
+          graph_comp_center = nx.center(graph_comp)
 
+          # Number of edges
+          graph_size = graph_comp.size()
+
+          # Number of nodes
+          graph_order = graph_comp.order()
+
+          # Average degree: size (nb of edges) / order (nb of nodes)
+          graph_avg_deg = float(graph_size) / graph_order
+
+          # Density
+          graph_density = nx.density(graph_comp)
+
+          # Degree: for every node, its number of edges
+          graph_degree = graph_comp.degree()
+
+          #
+          graph_centrality = nx.degree_centrality(graph_comp)
+
+          # DEBUG
+          print ("[" + airline_code + "]: size=" + str(graph_size)
+                 + ", order=" + str(graph_order)
+                 + ", density=" + str(graph_density)
+                 + ", degree=" + str(graph_degree))
+          if verboseFlag:
+              print ("[" + airline_code + "]: size=" + str(graph_size)
+                     + ", order=" + str(graph_order)
+                     + ", density=" + str(graph_density)
+                     + ", degree=" + str(graph_degree))
+
+              labelStr = "Sub-network[" + str(graph_comp_idx) + "] for " + airline_code
+              plt.figure(k_airline_idx + graph_comp_idx)
+              plt.title(labelStr)
+              nx.draw_networkx (graph_comp, optd_por_map_dict,
+                                node_color = 'green', label = labelStr)
+              # Draw the geographical features
+              basemap.drawcountries(linewidth = 0.5)
+              basemap.fillcontinents(color='white',lake_color='white')
+              basemap.drawcoastlines(linewidth=0.5)
+
+          #
+          center_node = graph_comp_center[0]
+          max_dist_to_center_km = 0
+          max_dist_node = center_node
+          sum_dist_km = 0
+          for idx_node in graph_comp:
+              # Distance of the current node to the center of the sub-network
+              dist_to_center = nx.shortest_path_length (graph_comp,
+                                                        center_node, idx_node)
+              dist_to_center_km = dq.geocalc (center_node, idx_node,
+                                              optd_por_coord_dict)
+          
+              sum_dist_km += dist_to_center_km
+
+              #
+              if dist_to_center_km > max_dist_to_center_km:
+                  max_dist_to_center_km = dist_to_center_km
+                  max_dist_node = idx_node
+
+
+          # Calculate the geographical distance statistics (average and max)
+          avg_dist_to_center_km = sum_dist_km / graph_comp.order()
+
+          # Calculate the ratio (max distance / avg distance)
+          ratio_dist = max_dist_to_center_km / avg_dist_to_center_km
+
+          # Reporting
+          airline_name = airline_dict[airline_code]['airline_name']
+          reportStruct = {'airline_code': airline_code,
+                          'airline_name': airline_name,
+                          'subnetwork_id': graph_comp_idx,
+                          'center_list': graph_comp_center,
+                          'center': center_node,
+                          'order': graph_order, 'size': graph_size,
+                          'avg degree': graph_avg_deg,
+                          'density': graph_density,
+                          'degree_list': graph_degree,
+                          'degree_centrality': graph_centrality,
+                          'max_node': max_dist_node,
+                          'max_dist': max_dist_to_center_km,
+                          'avg_dist': avg_dist_to_center_km,
+                          'ratio_dist': ratio_dist}
+
+          # DEBUG
+          if verboseFlag or ratio_dist >= 5.0:
+              print (str(reportStruct))
+
+          # Iteration on the sub-networks
+          graph_comp_idx += 1
+
+      # Iteration on the airlines
+      airline_idx += 1
+
+      # For the current airline
+      if verboseFlag:
+          plt.show()
