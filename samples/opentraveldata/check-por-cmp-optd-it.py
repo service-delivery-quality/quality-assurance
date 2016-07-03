11e2a513b1c4597a764c98bbe4d78cef5c6ce7a2
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2016-07-03 12:42:07 +0200

    [Samples][OPTD] The OPTD/IATA comparison takes into account execptions (optd_por_exceptions.csv)

--- samples/opentraveldata/check-por-cmp-optd-it.py
+++ samples/opentraveldata/check-por-cmp-optd-it.py
@@ -13,20 +13,47 @@ if __name__ == '__main__':
   optd_por_public_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/opentraveldata/optd_por_public.csv?raw=true'
   optd_por_public_file = 'to_be_checked/optd_por_public.csv'
 
+  # OPTD-maintained list of (POR, city) known issues
+  # for different sources (eg, reference, IATA, OAG, Innovata)
+  optd_por_exc_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/opentraveldata/optd_por_exceptions.csv?raw=true'
+  optd_por_exc_file = 'to_be_checked/optd_por_exceptions.csv'
+
   # IATA derived list of POR
   optd_por_it_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/data/IATA/iata_airport_list_latest.csv?raw=true'
   optd_por_it_file = 'to_be_checked/iata_airport_list_latest.csv'
 
   # If the files are not present, or are too old, download them
   dq.downloadFileIfNeeded (optd_por_public_url, optd_por_public_file, verboseFlag)
+  dq.downloadFileIfNeeded (optd_por_exc_url, optd_por_exc_file, verboseFlag)
   dq.downloadFileIfNeeded (optd_por_it_url, optd_por_it_file, verboseFlag)
 
   # DEBUG
   if verboseFlag:
     dq.displayFileHead (optd_por_public_file)
+    dq.displayFileHead (optd_por_exc_file)
     dq.displayFileHead (optd_por_it_file)
 
-  #
+  # Known errors, by various sources
+  por_exc_dict = dict()
+  with open (optd_por_exc_file, newline='') as csvfile:
+    file_reader = csv.DictReader (csvfile, delimiter='^')
+    for row in file_reader:
+      por_code = row['por_code']
+      cty_code = row['city_code']
+      exc_src = row['source']
+      env_id = row['env_id']
+      actv_in_optd = row['actv_in_optd']
+      actv_in_src = row['actv_in_src']
+
+      #
+      if not por_code in por_exc_dict and env_id == "" and "I" in exc_src and actv_in_src == "1" and actv_in_optd == "0":
+        # Register the error details for the POR
+        por_exc_dict[por_code] = {'city_code': cty_code, 'source': exc_src,
+                                  'actv_in_optd': actv_in_optd,
+                                  'actv_in_src': actv_in_src,
+                                  'used': False}
+
+  # OPTD-maintained POR with the full details
   optd_por_dict = dict()
   with open (optd_por_public_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
@@ -72,7 +99,7 @@ if __name__ == '__main__':
                                    'state_code': optd_state_code,
                                    'cty_list': city_code_list_str}
 
-  #
+  # IATA derived list of POR
   with open (optd_por_it_file, newline='') as csvfile:
     file_reader = csv.DictReader (csvfile, delimiter='^')
     for row in file_reader:
@@ -86,15 +113,27 @@ if __name__ == '__main__':
       #it_tz_code = row['tz_code']
       #it_loc_id = row['loc_id']
 
+      # Check whether the POR is known to be an error
+      # for the reference ("I") source
+      it_por_err = False
+      if it_por_code in por_exc_dict:
+        if "I" in por_exc_dict[it_por_code]['source'] and por_exc_dict[it_por_code]['actv_in_src'] == "1":
+          it_por_err = True
+
+          # Remove the record from the known errors, as it has been consumed
+          por_exc_dict[it_por_code]['used'] = True
+
       # Check whether the IATA derived POR is in the list of OPTD POR
-      if not it_por_code in optd_por_dict:
-        # The OPTD POR cannot be found in the list of IATA derived POR
+      if not it_por_code in optd_por_dict and not it_por_err:
+        # The OPTD POR cannot be found in the list of IATA derived POR,
+        # and it is not a known exception
         reasonStr = "IATA derived POR not in OpenTravelData"
         reportStruct = {'por_code': it_por_code, 'in_optd': 0, 'in_iata': 1}
         print (str(reportStruct))
 
-      else:
-        # Retrieve the details from OPTD
+      elif not it_por_err:
+        # The OPTD POR is in the list of IATA derived POR,
+        # and it is not a known exception: retrieve the details from OPTD
         optd_por_details = optd_por_dict[it_por_code]
 
         optd_env_id = optd_por_details['env_id']
@@ -130,6 +169,17 @@ if __name__ == '__main__':
                           'optd_feat_code': optd_feat_code,
                           'optd_page_rank': optd_page_rank}
           print (str(reportStruct))
+
+      else:
+        # The exception rule has been consumed; register it
+        por_exc_dict[it_por_code]['used'] = True
+
+  # Sanity check
+  for por_code_exc in por_exc_dict:
+    if not por_exc_dict[por_code_exc]['used'] and por_exc_dict[por_code_exc]['actv_in_src'] == "1":
+      reportStruct = {'por_code': por_code_exc,
+                      'actv_in_it': por_exc_dict[por_code_exc]['actv_in_src']}
+      print ("!!!!! Remaining entry of the file of IATA related known errors: " + str(reportStruct) + ". Please, remove that from the '" + optd_por_exc_url + "' file.")
 
   # DEBUG
   if verboseFlag:
